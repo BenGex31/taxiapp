a3a33aecf4ea327816fbe3a50d6d1f54a9c08205
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-19 16:58:26 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 /** @format */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as Font from "expo-font";
 
 import LoginScreen from "./src/screens/LoginScreen";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export default function App() {
+  const loadRessources = async () => {
+    try {
+      
+    } catch (error) {
+      console.error("error loading ressources", error);
+    }
+  };
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }} >
-        <Screen name="Login" component={LoginScreen} />
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name='Login' component={LoginScreen} />
       </Navigator>
     </NavigationContainer>
   );
