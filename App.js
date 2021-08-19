16fbe78a3d0c8a6115cdca8346a9efe53e44532b
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-19 17:07:36 +0200

    Update App.js
    Import Fonts

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as Font from "expo-font";
@@ -11,13 +11,28 @@ import LoginScreen from "./src/screens/LoginScreen";
 const { Navigator, Screen } = createStackNavigator();
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
   const loadRessources = async () => {
     try {
-      
+      await Font.loadAsync({
+        Poppins: require("./assets/fonts/Poppins-Regular.ttf"),
+        LeckerliOne: require("./assets/fonts/LeckerliOne-Regular.ttf"),
+      });
+      setLoading(false);
     } catch (error) {
       console.error("error loading ressources", error);
     }
   };
+  useEffect(() => {
+    loadRessources()
+  }, [])
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
