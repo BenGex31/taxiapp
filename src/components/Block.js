65bcd98f7259f625f3b398b6be15158c2a428e32
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-19 16:32:39 +0200

    Create Block.js

---
+++ src/components/Block.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Block = ({ children }) => {
+  return <View>{children}</View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default Block;
