853a298350fdda2f690704c790f9d18837c7b2cc
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-19 18:38:57 +0200

    Update helpers.js
    Login success

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,38 @@
 /** @format */
 
 import { Platform } from "react-native";
+import * as Google from "expo-google-app-auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const prefix = Platform.OS === "ios" ? "ios" : "md";
+
+const config = {
+  iosClientId: `510368521089-kc7vrucutpr56ptu27pc2a90juqotu1i.apps.googleusercontent.com`,
+  androidClientId: `510368521089-c5mu6uv9niqnlb2dnvogph7m9781p324.apps.googleusercontent.com`,
+  iosStandaloneAppClientId: `<YOUR_IOS_CLIENT_ID>`,
+  androidStandaloneAppClientId: `<YOUR_ANDROID_CLIENT_ID>`,
+};
+
+export const auth = async () => {
+  try {
+    const { user, type } = await Google.logInAsync(config);
+    //console.log("result", result);
+    if (type === "success") {
+      // stocker user dans la BDD
+      // stocker user dans mémoire interne
+      const { name, photoUrl, email } = user;
+      await AsyncStorage.setItem(
+        "user",
+        JSON.stringify({
+          name,
+          photoUrl,
+          email,
+        })
+      );
+      // naviger vers l'écran Home
+      console.log("naviguer vers Home");
+    }
+  } catch (error) {
+    console.error("error auth", error);
+  }
+};
