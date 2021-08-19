229738b309b1a6963f3d7d0409d043317db74164
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-19 18:41:49 +0200

    Create HomeScreen.js

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const HomeScreen = (props) => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
