7910f360c6ee27d9b2124c3749c39a2baf58c72b
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-19 17:37:07 +0200

    Update LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import Constants from "expo-constants";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -9,16 +9,22 @@ import Block from "../components/Block";
 import Title from "../components/Title";
 import { prefix } from "../utils/helpers";
 
+const { width } = Dimensions.get("window");
+
 const LoginScreen = () => {
-  const { container, icon } = styles;
+  const { container, icon, container_2, titleContainer } = styles;
   return (
     <View style={container}>
       <Block>
         <Ionicons style={icon} name={`${prefix}-car`} />
         <Title content='TAXI APP' size='big' />
       </Block>
-      <Title content='Authentification' size='small' />
-      <Title content='Google Connexion' size='medium' />
+      <View style={container_2}>
+        <View style={titleContainer}>
+          <Title content='Authentification' size='small' />
+          <Title content='Google Connexion' size='medium' />
+        </View>
+      </View>
     </View>
   );
 };
@@ -34,6 +40,18 @@ const styles = StyleSheet.create({
   icon: {
     fontSize: 80,
     color: "#fff",
+  },
+  container_2: {
+    flexGrow: 1,
+    width: width,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleContainer: {
+    width: width - 80,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "flex-start",
   },
 });
 
