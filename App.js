2c71586b3fe2cac3fe8046edab46a05a1d9db5b9
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-19 18:57:03 +0200

    Update App.js
    stay on HomeScreen

--- App.js
+++ App.js
@@ -8,11 +8,13 @@ import * as Font from "expo-font";
 
 import LoginScreen from "./src/screens/LoginScreen";
 import HomeScreen from "./src/screens/HomeScreen";
+import { renderInitialScreen } from "./src/utils/helpers";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export default function App() {
   const [loading, setLoading] = useState(true);
+  const [initialScreen, setInitialScreen] = useState("Login");
 
   const loadRessources = async () => {
     try {
@@ -20,6 +22,10 @@ export default function App() {
         Poppins: require("./assets/fonts/Poppins-Regular.ttf"),
         LeckerliOne: require("./assets/fonts/LeckerliOne-Regular.ttf"),
       });
+      const screen = await renderInitialScreen();
+      if (screen) {
+        setInitialScreen(screen);
+      }
       setLoading(false);
     } catch (error) {
       console.error("error loading ressources", error);
@@ -40,7 +46,9 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
+      <Navigator
+        initialRouteName={initialScreen}
+        screenOptions={{ headerShown: false }}>
         <Screen name='Login' component={LoginScreen} />
         <Screen name='Home' component={HomeScreen} />
       </Navigator>
