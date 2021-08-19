76e1d9f783a49ce2167c3cf80b3e54062a00c224
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-19 16:45:40 +0200

    Create Title.js

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Title = ({ content }) => {
+  return (
+    <View>
+      <Text>{content}</Text>
+    </View>
+  );
+};
+
+const styles= StyleSheet.create({
+    
+})
+
+export default Title;
