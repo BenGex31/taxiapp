ab7cde3b4fd90b0fd2e537312128231eeb666582
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-24 15:24:25 +0200

    Update PassengerScreen.js

--- src/screens/PassengerScreen.js
+++ src/screens/PassengerScreen.js
@@ -2,13 +2,25 @@
 
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
 const PassengerScreen = () => {
+  const { container } = styles;
   return (
-    <View>
+    <View style={container}>
       <Text>Passenger Screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+});
 
 export default PassengerScreen;
