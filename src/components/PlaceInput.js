54f70b5e960220341be0c1421af63515fce6ad62
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-24 16:43:48 +0200

    Update PlaceInput.js

--- src/components/PlaceInput.js
+++ src/components/PlaceInput.js
@@ -1,21 +1,55 @@
 /** @format */
 
 import React from "react";
-import { View, TextInput, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet, Dimensions } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { prefix } from "../utils/helpers";
 
+const { width } = Dimensions.get("window");
+
 const PlaceInput = () => {
+  const { container, icon, input, inputContainer } = styles;
   return (
-    <View>
-      <View>
-        <TextInput />
-        <Ionicons name={`${prefix}-search`} />
+    <View style={container}>
+      <View style={inputContainer}>
+        <TextInput style={input} />
+        <Ionicons style={icon} name={`${prefix}-search`} />
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    top: 20,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    width: width - 50,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  icon: {
+    fontSize: 25,
+    color: "#d6d6d6",
+  },
+  input: {
+    fontSize: 16,
+    color: "#303030",
+    maxWidth: "70%",
+    minWidth: "30%",
+    fontFamily: "Poppins",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+  },
+});
 
 export default PlaceInput;
