c44c68cd2905f4281d0cbf663f6dff066d16405d
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-24 15:12:06 +0200

    Create PassengerScreen.js

---
+++ src/screens/PassengerScreen.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const PassengerScreen = () => {
+  return (
+    <View>
+      <Text>Passenger Screen</Text>
+    </View>
+  );
+};
+
+export default PassengerScreen;
