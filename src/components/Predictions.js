86ea62d32cb468645d8e254510ed43861997322e
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-26 11:08:09 +0200

    Update Predictions.js

--- src/components/Predictions.js
+++ src/components/Predictions.js
@@ -4,14 +4,34 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
 const Predictions = ({ main_text, secondary_text, key }) => {
+  const { secondary, main, container } = styles;
   return (
-    <View key={key}>
-      <Text>{secondary_text}</Text>
-      <Text>{main_text}</Text>
+    <View style={container} key={key}>
+      <Text style={secondary}>{secondary_text}</Text>
+      <Text style={main}>{main_text}</Text>
     </View>
   );
 };
 
-const syles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    borderTopWidth: 1,
+    borderTopColor: "#F6F6F6",
+    padding: 5,
+  },
+  secondary: {
+    color: "#D6D6D6",
+    fontSize: 12,
+    fontWeight: "300",
+    fontFamily: "Poppins",
+  },
+  main: {
+    color: "#303030",
+    fontSize: 16,
+    fontWeight: "700",
+    fontFamily: "Poppins",
+  },
+});
 
 export default Predictions;
